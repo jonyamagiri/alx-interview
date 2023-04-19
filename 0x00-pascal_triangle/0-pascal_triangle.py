3acf2940303fee4e3f5bf68c4552248e962ca1cf
Joab Nyamagiri <jonyamagiri@gmail.com>
2023-04-19 07:50:40 +0300

    fix-pep8

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -22,8 +22,9 @@ def pascal_triangle(n):
         prev_row = triangle[-1]  # Get the previous row from the triangle
         new_row = [1]  # Initialize new row with 1 at the beginning
         for j in range(1, i):
-            # Calculate the value in the new row using the values from the previous row
-            new_row.append(prev_row[j-1] + prev_row[j])
+            # Calculate the value in the new row using the values from the
+            # previous row
+            new_row.append(prev_row[j - 1] + prev_row[j])
         new_row.append(1)  # Add 1 at the end of the new row
         triangle.append(new_row)  # Add the new row to the triangle
 
