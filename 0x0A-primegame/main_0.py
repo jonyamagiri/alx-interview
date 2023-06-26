39be3a2efc759b983ddc8db36417a081e8e2d0fd
Joab Nyamagiri <jonyamagiri@gmail.com>
2023-06-26 13:44:13 +0300

    primegame

--- 0x0A-primegame/main_0.py
+++ 0x0A-primegame/main_0.py
@@ -6,9 +6,20 @@ isWinner = __import__('0-prime_game').isWinner
 print("Winner: {}".format(isWinner(5, [2, 5, 1, 4, 3])))
 
 # other test cases:
-
 print("Winner: {}".format(isWinner(10, [5, 5, 5, 5, 5, 2, 2, 2, 2, 2])))
 print("Winner: {}".format(isWinner(4, [11, 30, 1, 7])))
 print("Winner: {}".format(isWinner(6, [1, 1, 0, 0, 1, 8])))
 print("Winner: {}".format(isWinner(0, [0])))
 print("Winner: {}".format(isWinner(-1, [10])))
+
+# results of all the tests:
+"""
+ubuntu@ubuntu-HP:~$ ./main_0.py
+Winner: Ben
+Winner: Maria
+Winner: Ben
+Winner: Ben
+Winner: None
+Winner: None
+ubuntu@ubuntu-HP:~$ 
+"""
